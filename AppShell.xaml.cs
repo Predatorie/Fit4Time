5513c9b5c075cf369aee1177d84624c0af1a225b
Mallen <Predatorie@live.com>
2023-09-15 19:51:17 -0400

    Initial Commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Fit4Time;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Fit4Time;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
